11daeb92d4e9fbf5c5f3de1754bff02ac69e68fe
Guilherme de Lemos <guilherme.eti@gmail.com>
2022-08-21 03:15:53 -0300

    Add Cypress as the standard e2e testing framework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'evergreen-web-ui'`, () => {
+  it(`should have as title 'Evergreen'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('evergreen-web-ui');
+    expect(app.title).toEqual('Evergreen');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('evergreen-web-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Evergreen app is running!');
   });
 });
